2b63a13ba07030c11853c8590e8b86a225744db8
juanchopala <juanchelo159@gmail.com>
2025-01-31 21:36:32 -0300

    cammbios varios

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /*
+  experimental: {
+  ppr: 'incremental'
+  }
+  comentado por que sino lo hago el projecto no me anda 
+  */
 };
 
 export default nextConfig;
